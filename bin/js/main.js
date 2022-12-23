996f342023f649de95a1c67614181adbb6e7cfb7
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2022-12-23 17:05:20 +0500

    added

--- bin/js/main.js
+++ bin/js/main.js
@@ -2,23 +2,16 @@ let cross = document.querySelector(".mobile-cross")
 let menu = document.querySelector(".header__menu-btn")
 let navbar = document.querySelector(".header__navbar")
 
-menu.addEventListener("click" , ()=>{
-    if (navbar.style.transform == "translateY(100%)") {
-        navbar.style.transform = "translateY(0%)"
-        document.querySelector("#pp-nav").style.opacity = 0
-
-    }else{
-        navbar.style.transform = "translateY(100%)"
-        document.querySelector("#pp-nav").style.opacity = 1
-
-    }
+menu.addEventListener("click", () => {
+    navbar.style.transform = "translateY(0%)"
+    document.querySelector("#pp-nav").style.opacity = 0
 })
 
-cross.addEventListener("click" , ()=>{
+cross.addEventListener("click", () => {
     if (navbar.style.transform == "translateY(100%)") {
         document.querySelector("#pp-nav").style.opacity = 0
         navbar.style.transform = "translateY(0%)"
-    }else{
+    } else {
         document.querySelector("#pp-nav").style.opacity = 1
         navbar.style.transform = "translateY(100%)"
     }
