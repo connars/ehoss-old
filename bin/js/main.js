d0110dbafa8773fab086dca0b41e5f2769cb48de
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2022-12-23 15:47:49 +0500

    bug fixed

--- bin/js/main.js
+++ bin/js/main.js
@@ -5,15 +5,19 @@ let navbar = document.querySelector(".header__navbar")
 menu.addEventListener("click" , ()=>{
     if (navbar.style.diplay == "none") {
         navbar.style.display = "none"
+        document.querySelector("#pp-nav").style.opacity = 1
     }else{
         navbar.style.display = "flex"
+        document.querySelector("#pp-nav").style.opacity = 0
     }
 })
 
 cross.addEventListener("click" , ()=>{
     if (navbar.style.diplay == "none") {
+        document.querySelector("#pp-nav").style.opacity = 0
         navbar.style.display = "flex"
     }else{
+        document.querySelector("#pp-nav").style.opacity = 1
         navbar.style.display = "none"
     }
 })
