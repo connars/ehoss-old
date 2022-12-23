59a2a94067aca44ea7baee90708271b4fb1f2816
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2022-12-23 18:18:15 +0500

    bug fixed

--- bin/js/main.js
+++ bin/js/main.js
@@ -3,16 +3,16 @@ let menu = document.querySelector(".header__menu-btn")
 let navbar = document.querySelector(".header__navbar")
 
 menu.addEventListener("click", () => {
-    navbar.style.transform = "translateY(0%)"
+    navbar.style.transform = "translateX(0%)"
     document.querySelector("#pp-nav").style.opacity = 0
 })
 
 cross.addEventListener("click", () => {
-    if (navbar.style.transform == "translateY(100%)") {
+    if (navbar.style.transform == "translateX(100%)") {
         document.querySelector("#pp-nav").style.opacity = 0
-        navbar.style.transform = "translateY(0%)"
+        navbar.style.transform = "translateX(0%)"
     } else {
         document.querySelector("#pp-nav").style.opacity = 1
-        navbar.style.transform = "translateY(100%)"
+        navbar.style.transform = "translateX(100%)"
     }
 })
