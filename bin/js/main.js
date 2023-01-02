832db5dee0a011ad1c94d2ef082a78d439725f70
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2023-01-02 16:41:19 +0500

    added video

--- bin/js/main.js
+++ bin/js/main.js
@@ -42,134 +42,18 @@ if (window.outerWidth <= 900) {
     }
 }
 
-const player = document.getElementById("player"),
-    video = document.getElementById("video"),
-    controls = document.getElementById("controls"),
-    play = document.getElementById("switch"),
-    time = document.getElementById("time"),
-    volume_bar = document.getElementById("volume-progress-bar"),
-    volume = document.getElementById("volume-bar"),
-    progress_bar = document.getElementById("progress-bar"),
-    current_bar = document.getElementById("current-bar"),
-    buffer_bar = document.getElementById("buffer-bar"),
-    gettime = document.getElementById("gettime");
+let play = document.querySelector(".aboutV__play")
 
-video.load();
-setTimeout(() => {
-    next();
-    controls.style.opacity = "0";
-    volume.style.width = `100%`;
-}, 2000);
+play.addEventListener("click" , ()=>{
+    video.play()
+})
 
-function next() {
-    video.paused
-        ? Math.floor(video.currentTime) > Math.floor(video.duration)
-            ? ((video.currentTime = 0), next())
-            : (video.play(), (play.innerHTML = '<img class="play-btn" src="./bin/img/play.svg">'))
-        : (video.pause(), (play.innerHTML = '<img class="play-btn" src="./bin/img/stop.svg">'));
-}
+video.addEventListener("play", () => {
+    let play = document.querySelector(".aboutV__play")
+    play.style.display = "none"
+})
 
-video.ontimeupdate = () => {
-    buffered();
-    current_bar.style.width = `${(video.currentTime / video.duration) * 97.5}%`;
-    time.innerHTML = `${hhmmss(Math.floor(video.currentTime))} / ${hhmmss(
-        Math.floor(video.duration)
-    )}`;
-};
-
-progress_bar.addEventListener("click", (e) => {
-    video.paused
-        ? ((video.currentTime =
-            (e.offsetX / progress_bar.clientWidth) * video.duration),
-            (current_bar.style.width = `${(e.offsetX / progress_bar.clientWidth) * 97.5
-                }%`),
-            (time.innerHTML = `${hhmmss(Math.floor(video.currentTime))} / ${hhmmss(
-                Math.floor(video.duration)
-            )}`),
-            setTimeout(() => {
-                next();
-            }, 300),
-            buffered())
-        : (next(),
-            (video.currentTime =
-                (e.offsetX / progress_bar.clientWidth) * video.duration),
-            (current_bar.style.width = `${(e.offsetX / progress_bar.clientWidth) * 97.5
-                }%`),
-            (time.innerHTML = `${hhmmss(Math.floor(video.currentTime))} / ${hhmmss(
-                Math.floor(video.duration)
-            )}`),
-            setTimeout(() => {
-                next();
-            }, 300),
-            buffered());
-});
-
-progress_bar.onmousemove = (e) => {
-    let pbX = progress_bar.clientWidth;
-    let gtX = gettime.clientWidth;
-    let elX = e.offsetX;
-    gettime.innerHTML = hhmmss(
-        Math.floor((elX / pbX) * video.duration) < 0
-            ? 0
-            : Math.floor((elX / pbX) * video.duration)
-    );
-    gettime.style.left =
-        elX > gtX
-            ? elX < pbX - gtX
-                ? `${elX}px`
-                : `${pbX - (gtX / 2 + 10)}px`
-            : `${gtX / 2}px`;
-};
-
-volume_bar.addEventListener("click", (e) => {
-    video.volume = (e.offsetX / volume_bar.clientWidth) * 1;
-    volume.style.width = `${(e.offsetX / volume_bar.clientWidth) * 100}%`;
-});
-
-controls.onmouseover = () => {
-    controls.style.opacity = "1";
-};
-
-controls.onmouseout = () => {
-    controls.style.opacity = "0";
-};
-
-progress_bar.onmouseover = () => {
-    gettime.style.opacity = "1";
-};
-
-progress_bar.onmouseout = () => {
-    gettime.style.opacity = "0";
-};
-
-player.onmousemove = () => { };
-
-function hhmmss(seconds) {
-    let minutes = Math.floor(seconds / 60);
-    seconds %= 60;
-    let hours = Math.floor(minutes / 60);
-    return ((minutes %= 60), 3600 <= video.duration)
-        ? `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`
-        : 3600 > video.duration
-            ? `${pad(minutes)}:${pad(seconds)}`
-            : void 0;
-}
-
-function pad(a) {
-    return ("0" + a).slice(-2);
-}
-
-function buffered() {
-    for (let index = 0; index < video.buffered.length; index++)
-        video.buffered.start(video.buffered.length - 1 - index) < video.currentTime
-            ? (buffer_bar.style.width = `${(video.buffered.end(video.buffered.length - 1 - index) /
-                    video.duration) *
-                97.5
-                }%`)
-            : false;
-}
-
-function px2percent(pixels, elementX) {
-    const screen = elementX ? elementX : window.screen.width;
-    return 100 - ((screen - pixels) / screen) * 100;
-}
+video.addEventListener("pause", () => {
+    let play = document.querySelector(".aboutV__play")
+    play.style.display = "block"
+})
